b236f6443c9c4ef59a89b2a1a53e042ad6ee9f0b
ayusxh009 <1raj.aayush@gmail.com>
2025-01-17 23:08:57 +0530

    Added About Page

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
+import About from './About';
 
 const HomePage = () => {
+  
   return (
+
     <div className="bg-gray-50 min-h-screen flex flex-col">
         
       <Header />
-      
       <Body />
       <Footer />
     </div>
